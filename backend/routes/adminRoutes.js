fee74eaedf93553c7afdd64b3f51220526a420f3
DeepikaRajapaksha <rpdsewwandi@std.appsc.sab.ac.lk>
2025-06-23 20:43:18 +0530

    Setup again

--- backend/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-// adminRoutes.js
-const express = require('express');
-const router = express.Router();
-const bookController = require('../controllers/bookController');
-
-// Route to handle adding a new book (admin only)
-router.post('/addBook', bookController.addBook);
-
-module.exports = router;
